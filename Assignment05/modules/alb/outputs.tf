ff1bd91d167f24534b8b7236a3d8f910648706b6
Sachin <92563151+Sachin1652@users.noreply.github.com>
2026-06-03 13:24:08 +0530

    Add outputs for ALB DNS name and Zone ID

---
+++ Assignment05/modules/alb/outputs.tf
@@ -0,0 +1,9 @@
+output "alb_dns_name" {
+  description = "DNS name of the ALB"
+  value       = aws_lb.this.dns_name
+}
+
+output "alb_zone_id" {
+  description = "Zone ID of the ALB"
+  value       = aws_lb.this.zone_id
+}
