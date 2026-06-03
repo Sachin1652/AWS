4cd94d7791fe698629e9f16c592a272f4a937007
Sachin <92563151+Sachin1652@users.noreply.github.com>
2026-06-03 13:31:41 +0530

    Add VPC peering resources in main.tf

---
+++ Assignment05/modules/vpc-peering /main.tf
@@ -0,0 +1,26 @@
+resource "aws_vpc_peering_connection" "this" {
+  vpc_id      = var.requester_vpc_id
+  peer_vpc_id = var.accepter_vpc_id
+  auto_accept = true
+
+  tags = merge(
+    var.tags,
+    {
+      Name = "VPC-peering"
+    }
+  )
+}
+
+resource "aws_route" "requester_to_accepter" {
+  count                     = length(var.requester_route_table_ids)
+  route_table_id            = var.requester_route_table_ids[count.index]
+  destination_cidr_block    = var.accepter_cidr
+  vpc_peering_connection_id = aws_vpc_peering_connection.this.id
+}
+
+resource "aws_route" "accepter_to_requester" {
+  count                     = length(var.accepter_route_table_ids)
+  route_table_id            = var.accepter_route_table_ids[count.index]
+  destination_cidr_block    = var.requester_cidr
+  vpc_peering_connection_id = aws_vpc_peering_connection.this.id
+}
